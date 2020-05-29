05f68d49fe39aa4f8e9e1f90d65500d62bff203f
syoyokoi0328 <s.yokoi@gemcook.com>
2020-05-29 10:06:51 +0900

    create crud-redux-api

--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+import "./index.css";
+import reducer from "./reducers/index";
+import EventsIndex from "./components/events_index";
+import EventsNew from "./components/events_new";
+import EventsShow from "./components/events_show";
+import * as serviceWorker from "./serviceWorker";
+
+const enhancer =
+  process.env.NODE_ENV === "development"
+    ? composeWithDevTools(applyMiddleware(thunk))
+    : applyMiddleware(thunk);
+const store = createStore(reducer, enhancer);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/events/new" component={EventsNew} />
+        <Route path="/events/:id" component={EventsShow} />
+        <Route exact path="/" component={EventsIndex} />
+        <Route exact path="/events" component={EventsIndex} />
+      </Switch>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
